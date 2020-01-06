b2cd5feb79e8267a4921caa25991e698cb5b0863
Harry Pierson <harrypierson@outlook.com>
2020-01-06 12:30:03 -0800

    latest SC FX + metadata attributes

--- templates/csharp/SmartContract1.cs
+++ templates/csharp/SmartContract1.cs
@@ -1,6 +1,13 @@
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 
+// [assembly: ContractTitle("optional contract title")]
+// [assembly: ContractDescription("optional contract description")]
+// [assembly: ContractVersion("optional contract version")]
+// [assembly: ContractAuthor("optional contract author")]
+// [assembly: ContractEmail("optional contract email")]
+[assembly: Features(ContractPropertyState.HasStorage)]
+
 public class SmartContract1 : SmartContract
 {
     public static void Main()
--- templates/visualbasic/SmartContract1.vb
+++ templates/visualbasic/SmartContract1.vb
@@ -1,6 +1,14 @@
 Imports Neo.SmartContract.Framework
 Imports Neo.SmartContract.Framework.Services.Neo
 
+' <Assembly: ContractTitle("optional contract title")>
+' <Assembly: ContractDescription("optional contract description")>
+' <Assembly: ContractVersion("optional contract version")>
+' <Assembly: ContractAuthor("optional contract author")>
+' <Assembly: ContractEmail("optional contract email")>
+<Assembly: Features(ContractPropertyState.HasStorage)>
+
+
 Public Class SmartContract1 : Inherits SmartContract
     Public Shared Sub Main()
         Storage.Put(Storage.CurrentContext, "Hello", "World")
