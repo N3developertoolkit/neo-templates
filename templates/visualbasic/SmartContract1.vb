fd869077c46c39acbf94369832bc08c23056e9e6
David Nicholson <me@djnicholson.com>
2020-03-03 15:59:11 +0000

    use multiple methods in template contracts

--- templates/visualbasic/SmartContract1.vb
+++ templates/visualbasic/SmartContract1.vb
@@ -1,3 +1,5 @@
+Imports System
+
 Imports Neo.SmartContract.Framework
 Imports Neo.SmartContract.Framework.Services.Neo
 
@@ -10,7 +12,24 @@ Imports Neo.SmartContract.Framework.Services.Neo
 
 
 Public Class SmartContract1 : Inherits SmartContract
-    Public Shared Sub Main()
-        Storage.Put(Storage.CurrentContext, "Hello", "World")
-    End Sub
+
+    Public Shared Function Main(method As String, args As Object()) As Byte()
+        If String.Equals(method, "PutValue") Then
+            Main = PutValue(args(0))
+        ElseIf String.Equals(method, "GetValue") Then
+            Main = GetValue()
+        Else
+            Throw new NotImplementedException()
+        End If
+    End Function
+
+    Public Shared Function PutValue(obj As Byte()) As Byte()
+        Storage.Put(Storage.CurrentContext, "StoredData", obj)
+        PutValue = obj
+    End Function
+
+    Public Shared Function GetValue() As Byte()
+        GetValue = Storage.Get(Storage.CurrentContext, "StoredData")
+    End Function
+
 End Class
